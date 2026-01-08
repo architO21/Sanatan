e7b0ff618bcef7ed078e7578b1adb2644f9825d9
Archit Bajpai <archit208027@gmail.com>
2026-01-08 14:48:34 +0530

    Revert "initial commits setting up the project"
    This reverts commit 4c473f64f0c7c5311eb6526ac9a01929e5b34c06.

--- shared/src/commonMain/kotlin/com/example/networkio/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.networkio
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- shared/src/commonMain/kotlin/com/example/networkio/network/JsonConfig.kt
+++
@@ -1,4 +0,0 @@
-package com.example.networkio.network
-
-class JsonConfig {
-}
